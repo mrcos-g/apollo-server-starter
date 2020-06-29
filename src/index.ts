7ea5bc572cf70a6a24d7ef4f695883dc41d051b9
Marcos Gonzalez <67390524+mrcos-g@users.noreply.github.com>
2020-06-28 20:27:37 -0400

    Include basic apollo server configuration from get starter tutorial (#3)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,39 @@
-const test = 'App bundled';
+import { ApolloServer, gql } from 'apollo-server';
 
-console.log(test);
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: 'J.K. Rowling',
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton',
+  },
+];
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const startServer = async () => {
+  const port = (process.env.PORT || 4000) as number;
+  const server = new ApolloServer({ typeDefs, resolvers });
+  server.listen(port).then(({ url }) => {
+    console.log(`Server running at ${url}`);
+  });
+};
+
+startServer();

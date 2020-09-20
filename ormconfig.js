9d343941514ced1d8559fd0d045c60899e255be5
mrcos-g <67390524+mrcos-g@users.noreply.github.com>
2020-09-20 09:42:38 -0400

    Add typeORM connection

--- ormconfig.js
+++ ormconfig.js
@@ -1,9 +1,10 @@
 var sharedConnectionOptions = {
-  host: 'localhost', // env
-  port: 4000, //env
-  username: 'postgres', //env
-  password: 'password', //env
-  database: 'demo', //env
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 5432,
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME || 'gql_starter',
+  synchronize: false,
   uuidExtension: 'pgcrypto',
   type: 'postgres',
 };
@@ -11,5 +12,17 @@ var sharedConnectionOptions = {
 module.exports = [
   {
     ...sharedConnectionOptions,
+    cli: {
+      entitiesDir: 'src/entities',
+      migrationsDir: 'src/migrations',
+    },
+    entities: ['src/entities/**/*.ts'],
+    migrations: ['src/migrations/**/*.ts'],
+  },
+  {
+    ...sharedConnectionOptions,
+    entities: ['build/bundle.js'],
+    migrations: ['build/migrations/**/*.js'],
+    name: 'build',
   },
 ];
